7e65b891e1f42b41d320a5b79dd6a1d1beb41d49
mohamedikhlaf27 <61310252+mohamedikhlaf27@users.noreply.github.com>
2020-06-02 15:47:29 +0200

    Winkelwagen aangemaakt

---
+++ BC House ASP/Controllers/WinkelwagenController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BC_House_ASP.Controllers
+{
+    public class WinkelwagenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
